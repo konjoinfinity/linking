bfbdd834619bc2893514ee41d53313110f2d2476
Wesley Scholl <clients@konjoweb.com>
2021-02-23 19:50:42 -0500

    updated the applink package to account for changes to ios appstore link changes

--- Open.js
+++ Open.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { Button } from 'react-native';
 import AppLink from 'react-native-app-link';
 
+const url = "lyft://"
+const appName = "lyft"
+const appStoreId = "529379082"
+const appStoreLocale = "il"
+const playStoreId = "me.lyft.android"
+
 class Open extends React.Component {
     constructor(props) {
         super(props);
@@ -14,15 +20,21 @@ class Open extends React.Component {
             // do stuff
         })
             .catch((err) => {
-                // handle error
+                console.log(err)
             });
 
-        AppLink.openInStore({ appName, appStoreId, appStoreLocale, playStoreId }).then(() => {
-            // do stuff
-        })
-            .catch((err) => {
-                // handle error
-            });
+        // maybeOpenURL(url, config) -> Promise Opens link if app is present. If not, it opens an app store to prompt the user to download it.
+        // url: (String) a url in the specified app's deep linking format that points to the content you want to open.
+
+        // config: (Object) a set of fallback urls if the app requested does not exist locally.
+
+        // config.appName: (String) the app's name you're linking into.
+
+        // config.appStoreId: (String) the app's ID on the App Store (iOS). Example: { appStoreId: '529379082' }
+
+        // config.appStoreLocale: (String) the App Store's locale (iOS). Defaults to the USA App Store. Example: { appStoreId: 'us' }
+
+        // config.playStoreId: (String) the app's package identifier on the Play Store (Android). Example: { playStoreId: 'me.lyft.android' }
     }
 
     render() {
